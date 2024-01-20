121765bde2e7cf8a4573b78add404e78adbbac57
Mash-Rafi-Rabid <rabidmashrafi@gmail.com>
2024-01-21 02:47:00 +0600

    first-git-repo

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text-js",{
+	strings:["FrontEnd Web Developer","Web Designer","Javascript Developer","BackEnd Developer"],
+	typeSpeed:100,
+	backSpeed:100,
+	backDelay:100,
+	loop:false
+});
